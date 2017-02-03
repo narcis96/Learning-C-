7fb5af7f7c7159a4161c7c6e85363c7640328de9
Narcis Gabriel Gemene <narcisgabriel.gemene@gameloft.com>
2017-02-03 16:41:35 +0200

    List Directories

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Collections;
+namespace Tasks
+{
+    class Program
+    {
+        static void DoStuff()
+        {
+            Console.WriteLine("DoStuff");
+        }
+        static ArrayList listFiles = new ArrayList();
+        static void ListDiretories(String path)
+        {
+            Action finishFilesTask = () =>
+            {
+                Task<string[]> taskListFiles = Task.Run(() =>
+                {
+                    return Directory.GetFiles(path);
+                });
+                taskListFiles.ContinueWith((antecedent) =>
+                {
+                    string[] files = antecedent.Result;
+                    foreach (string file in files)
+                    {
+                        lock (listFiles.SyncRoot)
+                        {
+                            listFiles.Add(file);
+                        }
+                    }
+                }).Wait();
+            };
+
+            Action finishDirectoryTask = () =>
+            {
+                Task<string[]> taskListDiretories = Task.Run(() =>
+                {
+                    return Directory.GetDirectories(path);
+                });
+                taskListDiretories.ContinueWith((antecedent) =>
+                {
+                    string[] directories = antecedent.Result;
+                    foreach (string directory in directories)
+                    {
+                        ListDiretories(directory);
+                    }
+                }).Wait();
+            };
+
+            Parallel.Invoke(finishFilesTask, finishDirectoryTask);
+        }
+
+        static void Main(string[] args)
+        {
+            ListDiretories(args[0]);
+            listFiles.Sort();
+            foreach(string file in listFiles)
+            {
+                Console.WriteLine(file);
+            }
+            Action doStuff = DoStuff;
+            Func<int> getOne = () => 1;
+
+            Func<int, int, string> convertIntToString = (i, j) => 
+            i.ToString() + " + " + j.ToString()+ " = " + (i + j).ToString();
+
+            Action<string> printToScreen = s => 
+            {
+                Console.WriteLine(s);
+            };
+            printToScreen(convertIntToString(getOne(),getOne()));
+        }
+    }
+}
---
+++ Threads/Threads/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threads
+{
+    class Helper
+    {
+        public Helper()
+        {
+            Console.WriteLine("Helper::Helper");
+        }
+        ~Helper()
+        {
+            Console.WriteLine("Helper::~Helper");
+        }
+        public void DoStuff()
+        {
+
+            Console.WriteLine("Helper::DoStuff");
+        }
+    }
+
+    class Program
+    {
+        static private bool stopped;
+
+        [ThreadStatic]
+        static private int _field;
+
+        public static void Func(object data)
+        {
+            int threadID = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Thread " + threadID + " starts");
+            int n = (int)data;
+            while (stopped == false)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    _field += 1;
+                    Console.WriteLine("ThreadProc " + threadID + " : " + i.ToString());
+                    Thread.Sleep(1000);
+                }
+            }
+            Console.WriteLine("Thread " + threadID + " ends");
+
+        }
+
+        static void Main(string[] args)
+        {
+            stopped = false;
+            {
+                Helper h = new Helper();
+                h.DoStuff();
+                h = null;
+            }
+            Thread firstThread = new Thread(Program.Func);
+
+            Thread secondThread = new Thread(new ThreadStart(() =>
+            {
+                int threadID = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine("Thread " + threadID + " starts");
+                while (true)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine("ThreadProc " + threadID + " : " + i.ToString());
+                        Thread.Sleep(1000);
+                    }
+                }
+                Console.WriteLine("Thread " + threadID + " ends");//never reach here
+            }));
+
+            Thread thirdThread = new Thread(new ParameterizedThreadStart((object data) =>
+            {
+                int threadID = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine("Thread " + threadID + " starts");
+                int n = (int)data;
+                //while (true)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine("ThreadProc " + threadID + " : " + i.ToString());
+                        Thread.Sleep(1000);
+                    }
+                }
+                Console.WriteLine("Thread " + threadID + " ends");
+            }));
+            new Thread((object data) =>
+            {
+                int threadID = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine("Thread " + threadID + " starts");
+                int n = (int)data;
+                for (int i = 0; i < n; i++)
+                {
+                    _field += 1;
+                    Console.WriteLine("ThreadProc " + threadID + " : " + i.ToString());
+                }
+                Console.WriteLine("Thread " + threadID + " ends");
+            }).Start(5);
+
+            firstThread.Start(5);
+
+            secondThread.IsBackground = true;
+            secondThread.Start();
+
+            thirdThread.Start(5);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            stopped = true;
+            firstThread.Join();
+            //secondThread.Join();
+            thirdThread.Join();
+            Thread.Sleep(3000);
+            Console.WriteLine("_field = " + _field);
+        }
+    }
+}
